a3858d283899451efd23b27c17b1055743c1e25b
fjetter <fjetter@users.noreply.github.com>
2021-07-08 15:47:49 +0200

    Remove explicit timeout for test_file_descriptors

--- distributed/tests/test_scheduler.py
+++ distributed/tests/test_scheduler.py
@@ -929,9 +929,7 @@ async def test_retire_workers_no_suspicious_tasks(c, s, a, b):
 
 
 @pytest.mark.slow
-@pytest.mark.skipif(
-    sys.platform.startswith("win"), reason="file descriptors not really a thing"
-)
+@pytest.mark.skipif(WINDOWS, reason="num_fds not supported on windows")
 @gen_cluster(client=True, nthreads=[], timeout=240)
 async def test_file_descriptors(c, s):
     await asyncio.sleep(0.1)
@@ -980,10 +978,8 @@ async def test_file_descriptors(c, s):
             assert comm.closed() or comm.peer_address != s.address, comm
     assert not s.stream_comms
 
-    start = time()
     while proc.num_fds() > num_fds_1 + N:
         await asyncio.sleep(0.01)
-        assert time() < start + 3
 
 
 @pytest.mark.slow
