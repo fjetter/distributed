fefa99177bdb3dc7ac9453d1c1ff593ff64c5e37
fjetter <fjetter@users.noreply.github.com>
2023-06-27 09:27:04 +0200

    Allow configuration

--- distributed/utils_test.py
+++ distributed/utils_test.py
@@ -36,6 +36,7 @@ from tornado.httpclient import AsyncHTTPClient
 from tornado.ioloop import IOLoop
 
 import dask
+from dask.utils import parse_bytes
 
 from distributed import Event, Scheduler, system
 from distributed import versions as version_module
@@ -2556,9 +2557,8 @@ class SizeOf:
     An object that returns exactly nbytes when inspected by dask.sizeof.sizeof
     """
 
-    def __init__(self, nbytes: int) -> None:
-        if not isinstance(nbytes, int):
-            raise TypeError(f"Expected integer for nbytes but got {type(nbytes)}")
+    def __init__(self, nbytes: int | str) -> None:
+        nbytes = parse_bytes(nbytes)
         if nbytes < _size_obj:
             raise ValueError(
                 f"Expected a value larger than {_size_obj} integer but got {nbytes}."
