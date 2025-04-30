59c2bcbe4a19bf371487944fc5e7b2e251d1ae32
fjetter <fjetter@users.noreply.github.com>
2025-04-30 11:05:19 +0200

    promote tls log to info

--- distributed/comm/tcp.py
+++ distributed/comm/tcp.py
@@ -443,7 +443,7 @@ class TLS(TCP):
         if sock is not None:
             self._extra.update(peercert=sock.getpeercert(), cipher=sock.cipher())
             cipher, proto, bits = self._extra["cipher"]
-            logger.debug(
+            logger.info(
                 "TLS connection with %r: protocol=%s, cipher=%s, bits=%d",
                 self._peer_addr,
                 proto,
